34d9d7e9bca420f0439492291ec337e96dd831d7
Bozic01 <144359668+Bozic01@users.noreply.github.com>
2025-02-24 13:39:15 +0100

    migrations
    base migrations

--- database/migrations/2025_02_24_123805_create_stores_table.php
+++ database/migrations/2025_02_24_123805_create_stores_table.php
@@ -11,11 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('stores', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->decimal('total_price');
-            $table->foreignId('category_id')->nullable()->constrained();
             $table->timestamps();
         });
     }
@@ -25,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('store');
     }
 };
