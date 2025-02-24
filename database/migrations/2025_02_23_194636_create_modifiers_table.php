dda6f8d7ac13d31ebc957f1b0533e0a5fa8c54d3
Bozic01 <144359668+Bozic01@users.noreply.github.com>
2025-02-24 14:58:16 +0100

    modifiers table

--- database/migrations/2025_02_23_194636_create_modifiers_table.php
+++ database/migrations/2025_02_23_194636_create_modifiers_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('side_dishes', function (Blueprint $table) {
+        Schema::create('modifiers', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
